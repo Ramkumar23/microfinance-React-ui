7a3a73b100a630484c86cbe350a4febc0e751e45
Ramkumar <ramk2394@gmail.com>
2018-02-20 17:46:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   webpack: (config, { dev }) => {
     config.module.rules.push({ test: /\.css$/, include: /node_modules/,  loader:  'style!css' });
     return config;
-  }
+  },
    exportPathMap: function() {
     return {
       '/': { page: '/' },
